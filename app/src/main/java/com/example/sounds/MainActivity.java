b1c3457973b09a24712d6f107a85178bddbcf91c
NSmith <nick_smith@ukr.net>
2020-08-19 21:22:12 +0300

    it works

--- app/src/main/java/com/example/sounds/MainActivity.java
+++ app/src/main/java/com/example/sounds/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.sounds;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView cat, dog;
+    private MediaPlayer catSound, dogSound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cat = (ImageView)findViewById(R.id.cat);
+        dog = (ImageView)findViewById(R.id.dog);
+
+        catSound = MediaPlayer.create(this, R.raw.meowing);
+        dogSound = MediaPlayer.create(this, R.raw.barking);
+
+        imageClick();
+    }
+
+    public void imageClick(){
+        cat.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        soundPlay(catSound);
+                    }
+                }
+        );
+
+        dog.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        soundPlay(dogSound);
+                    }
+                }
+        );
+    }
+
+    public void soundPlay(MediaPlayer sound){
+        sound.start();
     }
 }
